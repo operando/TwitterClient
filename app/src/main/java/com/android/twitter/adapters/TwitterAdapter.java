e6d5bc9770bdd30461bbb62444325b40c1cf410a
operando <okanoshinobu8@gmail.com>
2014-08-31 23:53:17 +0900

    Refactoring adapter

--- app/src/main/java/com/android/twitter/adapters/TwitterAdapter.java
+++ app/src/main/java/com/android/twitter/adapters/TwitterAdapter.java
@@ -1,81 +1,84 @@
 package com.android.twitter.adapters;
 
-import java.util.List;
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.twitter.R;
-import com.android.twitter.TwitterStatus;
+import com.android.twitter.model.TwitterStatus;
+
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
-*
-* Viewにセットするデータ項目へのアクセスを提供するクラス.
-*
-*/
-public class TwitterAdapter extends ArrayAdapter<TwitterStatus> {
+ * Viewにセットするデータ項目へのアクセスを提供するクラス.
+ */
+public class TwitterAdapter extends BindableAdapter<TwitterStatus> {
 
-	/** レイアウトXMLからViewを動的に生成するために使用. */
-	private LayoutInflater inflater;
-	/** タイムラインの情報が入ったList. */
-	private List<TwitterStatus> items;
+    /**
+     * レイアウトXMLからViewを動的に生成するために使用.
+     */
+    private LayoutInflater inflater;
+    /**
+     * タイムラインの情報が入ったList.
+     */
+    private List<TwitterStatus> items;
 
-	/**
-	 *
-	 * コンストラクタ.
-	 *
-	 * @param context
-	 *            コンテキスト
-	 * @param textViewResourceId
-	 *            ビューをインスタンス化するときに使用するTextViewを含むレイアウトファイルのリソースID
-	 * @param item
-	 *            タイムラインの情報が入ったList
-	 */
-	public TwitterAdapter(Context context, int textViewResourceId,
-			List<TwitterStatus> item) {
-		super(context, textViewResourceId, item);
-		this.items = item;
-		this.inflater = (LayoutInflater) context
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-	}
+    static class ViewHolder {
+        @InjectView(R.id.icon)
+        ImageView mIcon;
+        @InjectView(R.id.text_id)
+        TextView mTextId;
+        @InjectView(R.id.text_name)
+        TextView mTextName;
+        @InjectView(R.id.text_tweet)
+        TextView mTextTweet;
+        @InjectView(R.id.text_date)
+        TextView mTextDate;
 
-	/**
-	 *
-	 * 表示する内容をViewにセットする.
-	 *
-	 * @param position Viewの位置
-	 * @param convertView 前回使用したViewオブジェクト
-	 * @param parent 複数のView含む親View
-	 * @return View Viewオブジェクト
-	 */
-	public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder(View view) {
+            ButterKnife.inject(this, view);
+        }
+    }
 
-		View view = convertView;
-		if (view == null) {
-			view = inflater.inflate(R.layout.tl, null);
-		}
+    /**
+     * コンストラクタ.
+     *
+     * @param context            コンテキスト
+     * @param textViewResourceId ビューをインスタンス化するときに使用するTextViewを含むレイアウトファイルのリソースID
+     * @param item               タイムラインの情報が入ったList
+     */
+    public TwitterAdapter(Context context, int textViewResourceId,
+                          List<TwitterStatus> item) {
+        super(context, item);
+        this.items = item;
+        this.inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
 
-		TwitterStatus item = items.get(position);
+    @Override
+    public View newView(LayoutInflater inflater, int position, ViewGroup container) {
+        View v = inflater.inflate(R.layout.tl, container, false);
+        ViewHolder vh = new ViewHolder(v);
+        v.setTag(vh);
 
-		if (item != null) {
+        return v;
+    }
 
-			TextView idView = (TextView) view.findViewById(R.id.text_id);
-			TextView dateView = (TextView) view.findViewById(R.id.text_date);
-			TextView tweetView = (TextView) view.findViewById(R.id.text_tweet);
-			ImageView iconView = (ImageView) view.findViewById(R.id.icon);
-			TextView nameView = (TextView) view.findViewById(R.id.text_name);
+    @Override
+    public void bindView(TwitterStatus item, int position, View view) {
+        ViewHolder vh = (ViewHolder) view.getTag();
 
-			idView.setText(item.getId());
-			tweetView.setText(item.getTl());
-			iconView.setImageBitmap(item.getIcon());
-			nameView.setText(item.getName());
-			dateView.setText(item.getDate());
-		}
-		return view;
-	}
+        vh.mTextId.setText(item.getId());
+        vh.mTextTweet.setText(item.getTl());
+        vh.mIcon.setImageBitmap(item.getIcon());
+        vh.mTextName.setText(item.getName());
+        vh.mTextDate.setText(item.getDate());
+    }
+
 }
