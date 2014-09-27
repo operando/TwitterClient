5281dcc6ec2d87e5dd0fde3d3e9cf2893ce24311
operando <okanoshinobu8@gmail.com>
2014-09-27 09:56:02 +0900

    Create Application Class

---
+++ app/src/main/java/com/android/twitter/TwitterClientApplication.java
@@ -0,0 +1,11 @@
+package com.android.twitter;
+
+import android.app.Application;
+
+public class TwitterClientApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
