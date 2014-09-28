286dc9fed50c1c47d4373b9a4b5cf3f85aa915e3
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2014-09-28 18:49:38 +0900

    Update PreferenceUtils

--- app/src/main/java/com/android/twitter/utils/PreferenceUtils.java
+++ app/src/main/java/com/android/twitter/utils/PreferenceUtils.java
@@ -1,7 +1,37 @@
 package com.android.twitter.utils;
 
-/**
- * Created by shinobu on 2014/09/28.
- */
-public class PreferenceUtils {
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public final class PreferenceUtils {
+
+    private PreferenceUtils() {
+    }
+
+    private static SharedPreferences getSharedPreferences(Context context, String preferenceName) {
+        return context.getSharedPreferences(preferenceName, Context.MODE_PRIVATE);
+    }
+
+    private static SharedPreferences.Editor getEditor(Context context, String preferenceName) {
+        return getSharedPreferences(context, preferenceName).edit();
+    }
+
+    public static void saveString(Context context, String preferenceName, String key, String value) {
+        SharedPreferences.Editor editor = getEditor(context, preferenceName);
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    public static String getString(Context context, String preferenceName, String key) {
+        return getString(context, preferenceName, key, null);
+    }
+
+    public static String getString(Context context, String preferenceName, String key, String defaultValue) {
+        return getSharedPreferences(context, preferenceName).getString(key, defaultValue);
+    }
+
+    public static void clear(Context context, String preferenceName) {
+        getEditor(context, preferenceName).clear().apply();
+    }
+
 }
