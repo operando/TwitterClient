9a1458f228e87c50760cab025ba2ef2eff1ed59e
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2014-09-28 18:49:20 +0900

    Update ToastUtils

--- app/src/main/java/com/android/twitter/utils/ToastUtils.java
+++ app/src/main/java/com/android/twitter/utils/ToastUtils.java
@@ -1,7 +1,18 @@
 package com.android.twitter.utils;
 
-/**
- * Created by shinobu on 2014/09/28.
- */
-public class ToastUtils {
+import android.content.Context;
+import android.widget.Toast;
+
+public final class ToastUtils {
+
+    private ToastUtils() {
+    }
+
+    public static void show(Context context, int resId) {
+        show(context, context.getString(resId));
+    }
+
+    public static void show(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
 }
